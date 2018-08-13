59ddef78e9e868c397f7f49a61452da87ab58634
rajvaibhavdubey <17dec004@lnmiit.ac.in>
2018-08-14 05:16:05 +0530

    restro database added

--- models/restro.js
+++ models/restro.js
@@ -6,6 +6,7 @@ const restroSchema = mongoose.Schema({
     state: {type: String, unique:true},
     country: {type: String, unique:true},
     rating: {type: Number, unique:true},
+    image: {type: String, unique: true},
     menu: [{
         name:{type:String, unique:true},
         type: {type:String, unique:true},
